3a153a83f05f286c9e69f3e0b5ac7050e5824fe1
chicha099 <tdsserafin@gmail.com>
2022-02-03 17:42:28 -0300

    added chichat and slay

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,67 +6,83 @@ import { useSelector } from "react-redux";
 import leaf from "../../Assets/Projects/leaf.png";
 import clima from "../../Assets/Projects/clima.png";
 import food from "../../Assets/Projects/food.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import slay from "../../Assets/Projects/slay.png";
+import chichat from "../../Assets/Projects/chichat.png";
 import poke from "../../Assets/Projects/poke.png";
 
 function Projects() {
-  const len = useSelector(state => state.len);
+  const len = useSelector((state) => state.len);
 
+  return len === "ES" ? (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          En que he estado <strong className="purple">Trabajando </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Aqui hay algunos de los proyectos en los que trabaje recientemente.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Coding To Help"
+              description="Un gran proyecto realizado en un equipo de 8 desarrolladores. El objetivo de CTL es ayudar a las pequeñas ONG a crecer y ganar visibilidad permitiéndoles recibir donaciones y difundir su filosofía."
+              link="https://github.com/MDVeloper/ong-client"
+            />
+          </Col>
 
-  return (
-    len === "ES" ?
-      <Container fluid className="project-section">
-        <Particle />
-        <Container>
-          <h1 className="project-heading">
-            En que he estado <strong className="purple">Trabajando </strong>
-          </h1>
-          <p style={{ color: "white" }}>
-            Aqui hay algunos de los proyectos en los que trabaje recientemente.
-          </p>
-          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={leaf}
-                isBlog={false}
-                title="Coding To Help"
-                description="Un gran proyecto realizado en un equipo de 8 desarrolladores. El objetivo de CTL es ayudar a las pequeñas ONG a crecer y ganar visibilidad permitiéndoles recibir donaciones y difundir su filosofía."
-                link="https://github.com/MDVeloper/ong-client"
-              />
-            </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={clima}
+              isBlog={false}
+              title="Heavy Weather"
+              description="La aplicación Heavy Weather proporciona una previsión meteorológica internacional y local de las ciudades, así como una geolocalización en tiempo real."
+              link="https://heavy-weather.vercel.app/"
+            />
+          </Col>
 
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={clima}
-                isBlog={false}
-                title="Heavy Weather"
-                description="La aplicación Heavy Weather proporciona una previsión meteorológica internacional y local de las ciudades, así como una geolocalización en tiempo real."
-                link="https://heavy-weather.vercel.app/"
-              />
-            </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={food}
+              isBlog={false}
+              title="Tasty Meals Done Dirty Cheap"
+              description="Una SPA que utiliza la API de Spoonacular para proporcionar información sobre +5000 recetas y permite buscar, aprender y también publicar nuevas recetas."
+              link="https://github.com/chicha099/Tasty-Meals-Done-Dirty-Cheap"
+            />
+          </Col>
 
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={food}
-                isBlog={false}
-                title="Tasty Meals Done Dirty Cheap"
-                description="Una SPA que utiliza la API de Spoonacular para proporcionar información sobre +5000 recetas y permite buscar, aprender y también publicar nuevas recetas."
-                link="https://github.com/chicha099/Tasty-Meals-Done-Dirty-Cheap"
-              />
-            </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={poke}
+              isBlog={false}
+              title="Gotta Map 'em All"
+              description="Básicamente una Pokedex que también te permite crear un nuevo Pokemons."
+              link="https://github.com/chicha099/Gotta-Map--em-All"
+            />
+          </Col>
 
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={poke}
-                isBlog={false}
-                title="Gotta Map 'em All"
-                description="Básicamente una Pokedex que también te permite crear un nuevo Pokemons."
-                link="https://github.com/chicha099/Gotta-Map--em-All"
-              />
-            </Col>
-
-            {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chichat}
+              isBlog={false}
+              title="ChiChat"
+              description="Una aplicación web de mensajería instantánea y grupos como Slack o Discord hecha sobre Reactjs utilizando CometChat y Firebase para el manejo de mensajes y usuarios respectivamente."
+              link="https://chi-chat.vercel.app/login"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={slay}
+              isBlog={false}
+              title="RogueLike insipirado en Slay The Spire (En progreso)"
+              description="React no está pensado para hacer videojuegos pero quise tomar el desafío de intentarlo. Aun está en progreso pero ya puedes luchar contra enemigos nuevos en cada sala, aplicar cambios de estado y algunas funcionalidades mas."
+              link="https://github.com/chicha099/card-game"
+            />
+          </Col>
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
@@ -86,61 +102,80 @@ function Projects() {
               link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
             />
           </Col> */}
-          </Row>
-        </Container>
+        </Row>
       </Container>
-      :
-      <Container fluid className="project-section">
-        <Particle />
-        <Container>
-          <h1 className="project-heading">
-            My Recent <strong className="purple">Works </strong>
-          </h1>
-          <p style={{ color: "white" }}>
-            Here are a few projects I've worked on recently.
-          </p>
-          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={leaf}
-                isBlog={false}
-                title="Coding To Help"
-                description="A big project done in an 8 developers team. CTL's goal is to help small NGOs to grow up and gain visibility by letting them receive donations and spread their philosophy."
-                link="https://github.com/MDVeloper/ong-client"
-              />
-            </Col>
+    </Container>
+  ) : (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Coding To Help"
+              description="A big project done in an 8 developers team. CTL's goal is to help small NGOs to grow up and gain visibility by letting them receive donations and spread their philosophy."
+              link="https://github.com/MDVeloper/ong-client"
+            />
+          </Col>
 
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={clima}
-                isBlog={false}
-                title="Heavy Weather"
-                description="The Heavy Weather app provide an international and local weather forecast for cities, as well as a real-time geolocalization."
-                link="https://heavy-weather.vercel.app/"
-              />
-            </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={clima}
+              isBlog={false}
+              title="Heavy Weather"
+              description="The Heavy Weather app provide an international and local weather forecast for cities, as well as a real-time geolocalization."
+              link="https://heavy-weather.vercel.app/"
+            />
+          </Col>
 
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={food}
-                isBlog={false}
-                title="Tasty Meals Done Dirty Cheap"
-                description="A SPA that uses the Spoonacular API to provide information about +5000 recipes and allows to search, learn and also post new recipes."
-                link="https://github.com/chicha099/Tasty-Meals-Done-Dirty-Cheap"
-              />
-            </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={food}
+              isBlog={false}
+              title="Tasty Meals Done Dirty Cheap"
+              description="A SPA that uses the Spoonacular API to provide information about +5000 recipes and allows to search, learn and also post new recipes."
+              link="https://github.com/chicha099/Tasty-Meals-Done-Dirty-Cheap"
+            />
+          </Col>
 
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={poke}
-                isBlog={false}
-                title="Gotta Map 'em All"
-                description="Basically a Pokedex that also let you can create a new Pokemons"
-                link="https://github.com/chicha099/Gotta-Map--em-All"
-              />
-            </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={poke}
+              isBlog={false}
+              title="Gotta Map 'em All"
+              description="Basically a Pokedex that also let you can create a new Pokemons"
+              link="https://github.com/chicha099/Gotta-Map--em-All"
+            />
+          </Col>
 
-            {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chichat}
+              isBlog={false}
+              title="ChiChat"
+              description="A web application for instant messaging and groups like Slack or Discord built on Reactjs using CometChat and Firebase for message and user management respectively.."
+              link="https://chi-chat.vercel.app/login"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={slay}
+              isBlog={false}
+              title="Slay The Spire based RogueLike (Work In Progress)"
+              description="React is not intended to make video games but I wanted to take the challenge to try it. It's still in progress but you can already fight new enemies in each room, apply state changes and some more features."
+              link="https://github.com/chicha099/card-game"
+            />
+          </Col>
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
@@ -160,9 +195,9 @@ function Projects() {
               link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
             />
           </Col> */}
-          </Row>
-        </Container>
+        </Row>
       </Container>
+    </Container>
   );
 }
 
